e909fc75d0c37fee5fd88116f22b5a99f8904d4e
raybarkerrr <raybarkerrr@gmail.com>
2023-04-26 16:35:41 -0500

    prints random number to console

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//let userInput = prompt("Rock, Paper, or Scissors?");
+
+let computerChoice = Math.ceil(Math.random() );
+
+
+
+console.log(computerChoice);
+
+function playRound(playerSelection, computerSelection) {
+
+  if (playerSelection === computerSelection) {
+    console.log("monkey");
+  } else{
+    console.log("wrong")
+  }
+
+
+
+  }
+   
